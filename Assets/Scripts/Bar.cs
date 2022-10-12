f377296b43235dadf20e3a473648ebf0e2ea4881
Vis <Vish106@rambler.ru>
2022-10-12 10:46:54 +0300

    Рeализован Slider

---
+++ Assets/Scripts/Bar.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bar : MonoBehaviour
+{
+    [SerializeField] private Slider _slider;
+    [SerializeField] private Player _player;
+
+    private float _waitForSecondsInterval = 0.01f;
+    private float deltaHealth = 1f;
+    private Coroutine _setTargetHealth;
+
+    public void OnValueChanged(float value, float maxValue)
+    {
+        float targetValue = value / maxValue;
+        SetTargetHealth(deltaHealth, targetValue, _waitForSecondsInterval);     
+    }
+
+    private void OnEnable()
+    {
+        _player.HealthChanged += OnValueChanged;
+        _slider.value = _player.GetStartHealth();
+    }
+
+    private void OnDisable()
+    {
+        _player.HealthChanged -= OnValueChanged;
+    }
+
+    private IEnumerator ChangeHealthCorutine(float deltaHealth, float targetHealth, float waitForSecondsInterval)
+    {
+        var timeInterval = new WaitForSeconds(waitForSecondsInterval);
+       
+        Debug.LogError(targetHealth);
+
+        while (_slider.value != targetHealth)
+        {
+            _slider.value = Mathf.MoveTowards(_slider.value, targetHealth, deltaHealth * Time.deltaTime);
+            Debug.Log(_slider.value);
+
+            yield return timeInterval;
+        }
+    }
+
+    private void SetTargetHealth(float deltaHealth, float targetHealth, float waitForSecondsInterval)
+    {
+        if (_setTargetHealth != null)
+            StopCoroutine(_setTargetHealth);
+
+        _setTargetHealth = StartCoroutine(ChangeHealthCorutine(deltaHealth, targetHealth, waitForSecondsInterval));
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float _maxHealth = 100;
+    [SerializeField] private float _currentHealth = 50;       
+
+    public event UnityAction<float, float> HealthChanged;
+
+    public void ChangeHealth(float deltaHealth)
+    {
+        float targetHealth = _currentHealth + deltaHealth;      
+        _currentHealth += deltaHealth;
+        HealthChanged?.Invoke(_currentHealth, _maxHealth);
+    }
+
+    public float GetStartHealth()
+    {
+        return _currentHealth / _maxHealth;
+    }
+}
